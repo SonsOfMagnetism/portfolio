2a924194e8cc62f4294bd5c5f6d4c0fb94f61d5d
Rene Santiago <rsj510@gmail.com>
2023-03-08 21:41:20 -0500

    complete skeleton

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import Header from "./components/Header"
 import Bio from "./components/Bio" 
+import Projects from "./components/Projects"
 
 function App() {
 
@@ -7,6 +8,7 @@ function App() {
     <div className="App">
       <Header/>
       <Bio/>
+      <Projects/>
     </div>
   )
 }
